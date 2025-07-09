391c1071f6f59e4e61e900f2cec08766b201b94d
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-07-09 18:49:40 +0530

    added BandNameGenerator.py

---
+++ ch1-variables/BandNameGenerator.py
@@ -0,0 +1,9 @@
+#PROJECT 2
+#1-greet the user
+#2-ask the user their favorite color and animal
+#3-generate their band's name
+
+print("Welcome to Band Name Generator! \n")
+color = input("What's your favorite color? \n")
+animal = input("What's your favorite animal? \n")
+print("Your Band Name could be: " + color + " " + animal + "s")
