4d375386fa2003245c03c882c627e41fc257246e
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-07-09 19:11:13 +0530

    TempConverter.py added

---
+++ ch1-variables/TempConverter.py
@@ -0,0 +1,7 @@
+#PROJECT 4
+#1-input temp in celsius from the user
+#2-convert it into Fahrenheit and print 
+
+tempinC = input("Enter the temperature in C")
+tempinF = (int(tempinC)*9/5)+32
+print (f" {tempinC} C = {tempinF} F")
