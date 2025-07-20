c8e002757104397d9f7e892a40dcbcbd8cffe452
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-07-20 11:43:00 +0530

    added LeapYear.py

---
+++ ch2-conditional-statements/LeapYear.py
@@ -0,0 +1,14 @@
+#PROGRAM 7
+#takes a year as an input and checks if the year is leap year or not
+
+year=int(input("Enter a year- "))
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print(f"It is a leap year.")
+        else:
+            print(f"Not a leap year.")
+    else:
+        print(f"It is a leap year.")
+else:
+    print(f"Not a leap year.")
