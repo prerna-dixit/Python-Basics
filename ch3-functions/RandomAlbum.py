be574c0bb0824decf6c3de84f3830c10931d78ff
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-08-12 15:50:01 +0530

    added RandomAlbum.py

---
+++ ch3-functions/RandomAlbum.py
@@ -0,0 +1,27 @@
+#uses random module and gives you a random taylor swift album based on it
+import random
+n=random.randint(1, 12)
+if n==1:
+    print("You got the debut album-- TAYLOR SWIFT")
+elif n==2:
+    print("You're as brave as the album-- FEARLESS")
+elif n==3:
+    print("You've got the courage to-- SPEAK NOW")
+elif n==4:
+    print("Here's your sad girl autumn-- RED")
+elif n==5:
+    print("Welcome to New York with-- 1989")
+elif n==6:
+    print("Don't we just love snakes-- REPUTATION")
+elif n==7:
+    print("It's golden-- LOVER")
+elif n==8:
+    print("Back to the cabin in the woods in august-- FOLKLORE")
+elif n==9:
+    print("We gettin the saddest-- EVERMORE")
+elif n==10:
+    print("You can still make the whole place shimmer!~-- MIDNIGHTS")
+elif n==11:
+    print("In our poet era-- THE TORTURED POETS DEPARTMENT")
+else:
+    print("YOU GOT THE LATEST!!!!--- THE LIFE OF A SHOWGIRL")
