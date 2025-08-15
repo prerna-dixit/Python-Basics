1355e5c0115fd7339bb005a474df3a27d7e96f07
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-08-15 18:33:39 +0530

    added LeapYearFunction.py

---
+++ ch3-functions/LeapYearFunction.py
@@ -0,0 +1,20 @@
+#PROJECT 11
+#Rewrite leap year project with function
+
+year = int (input ("Enter year: "))
+def LeapYear(year):
+    if year%4==0:
+        print( f"{year} is a leap year." )
+        if year%100==0:
+            print ( f"{year} is not a leap year.")
+            if year%400==0:
+                print(f"{year} is a leap year.")
+            else:
+                print(f"{year} is not a leap year.")
+        else:
+            print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+        
+
+LeapYear(year)
